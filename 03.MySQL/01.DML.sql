1a474686b54a6072a226c90b0cb9aba51b50f355
hiyeap <ynaa550@gmail.com>
2022-09-23 17:43:58 +0900

    -

--- 03.MySQL/01.DML.sql
+++ 03.MySQL/01.DML.sql
@@ -90,7 +90,7 @@ FROM city
 WHERE countrycode='KOR' 
 ORDER BY district, population DESC;
 
-# 함수
+/* 함수 */
 # count(*) - 건수
 SELECT COUNT(*) 
 FROM city 
@@ -132,20 +132,20 @@ SELECT GROUP_CONCAT(DISTINCT district)
 FROM city 
 WHERE countrycode='KOR';
 
-# 광역시도별 도시의 갯수가 많은 순서로
+# 광역시도별 도시의 개수가 많은 순서로
 SELECT district, COUNT(*) 
 FROM city 
 WHERE countrycode='KOR'
 GROUP BY district 
 ORDER BY COUNT(*) DESC, district;
 
-# 광역시도별 도시의 갯수가 5개 이상
+# 광역시도별 도시의 개수가 5개 이상
 SELECT district, COUNT(*) 
 FROM city 
 WHERE countrycode='KOR'
 GROUP BY district HAVING COUNT(*)>=5;
 
-# 광역시도별 도시의 갯수가 5개 이상을 내림차순으로 정렬
+# 광역시도별 도시의 개수가 5개 이상을 내림차순으로 정렬
 SELECT district, COUNT(*) 
 FROM city 
 WHERE countrycode='KOR'
@@ -153,15 +153,15 @@ GROUP BY district
 HAVING COUNT(*)>=5
 ORDER BY COUNT(*) DESC;
 
-# 도시의 갯수가 100개 이상인 국가를 도시갯수 내림차순으로 정렬
+# 도시의 갯수가 100개 이상인 국가를 도시개수 내림차순으로 정렬
 SELECT countrycode, COUNT(*)
 FROM city
 GROUP BY countrycode 
 HAVING COUNT(*)>=100 
 ORDER BY COUNT(*) DESC;
 
-/* 갯수 */
-# 도시의 갯수가 많은 5개 국가코드
+/* 개수 */
+# 도시의 개수가 많은 5개 국가코드
 SELECT countrycode, COUNT(*) 
 FROM city
 GROUP BY countrycode 
@@ -196,3 +196,26 @@ SELECT country.Name, city.Name, city.Population
 FROM city JOIN country 
 ON city.CountryCode=country.Code
 ORDER BY city.Population DESC LIMIT 10;
+
+# 대륙별로 국가숫자, GNP의 합, 평균 국가별 GNP는?
+SELECT continent, round(SUM(gnp),-3) as gnpSum,round(SUM(gnp),-3) AS gnpAvg
+FROM country
+GROUP BY continent;
+
+# 아시아 대륙에서 인구가 가장 많은 도시 10개를 내림차순으로 보여줄 것
+# (대륙명, 국가명, 도시명, 인구수)
+SELECT country.Continent, country.Name, city.Name, city.Population
+FROM country JOIN city
+WHERE continent = 'Asia'
+AND country.Code = city.countrycode
+ORDER BY population DESC
+LIMIT 10;
+
+# 전 세계에서 인구가 가장 많은 10개 도시에서 사용하는 공식언어는?
+# (도시명, 인구수, 언어명)
+SELECT C.name, c.population, l.Language
+FROM city C JOIN countrylanguage l 
+ON c.CountryCode = l.CountryCode
+WHERE l.IsOfficial = 'T'
+ORDER BY c.Population DESC
+LIMIT 10;
