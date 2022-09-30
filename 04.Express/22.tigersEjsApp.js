e67ef78a5d002f3ef19863ce916497f66c5e2747
hiyeap <ynaa550@gmail.com>
2022-09-30 12:48:30 +0900

    -

---
+++ 04.Express/22.tigersEjsApp.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const dm = require('./db/tigers-module');
+
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    dm.getList(rows => {
+        ejs.renderFile('views/22.index.ejs', { rows: rows }, (err, html) => {
+            res.send(html);
+        });
+    });
+});
+
+app.get('/create', (req, res) => {
+    ejs.renderFile('views/22.create.ejs', (err, html) => {
+        res.send(html);
+    });
+});
+
+app.post('/create', (req, res) => {
+    const player = req.body.player;
+    const backNo = parseInt(req.body.backNo);
+    const position = req.body.position;
+    dm.insertPlayer([player, backNo, position], () => {
+        res.redirect('/');
+    });
+});
+
+app.get('/update', (req, res) => {        // http://localhost:3000/update?id=123
+    const id = parseInt(req.query.id);
+    dm.getPlayer(id, rows => {
+        const player = rows[0].player;
+        const backNo = parseInt(rows[0].backNo);
+        const position = rows[0].position;
+        ejs.renderFile('views/22.update.ejs', {
+            id, player, backNo, position
+        }, (err, html) => {
+            res.send(html);
+        });
+    });
+});
+
+app.post('/update', (req, res) => {
+    const player = req.body.player;
+    const backNo = parseInt(req.body.backNo);
+    const position = req.body.position;
+    const id = parseInt(req.body.id);
+    dm.updatePlayer([player, backNo, position, id], () => {
+        res.redirect('/');
+    });
+});
+
+app.get('/delete', (req, res) => {        // http://localhost:3000/delete?id=123
+    const id = parseInt(req.query.id);
+    ejs.renderFile('views/22.delete.ejs', {
+        id
+    }, (err, html)=>{
+        res.send(html);
+    });
+});
+
+app.get('/deleteConfirm', (req, res) => {
+    const id = parseInt(req.query.id);
+    dm.deletePlayer(id, () => {
+        res.redirect('/');
+    });
+});
+
+app.get("*", (req, res) => {
+    res.status(404).send('Path not found.');
+});
+
+app.listen(3000, (req, res) => {
+    console.log('Server is running at http://127.0.0.1:3000');
+});
